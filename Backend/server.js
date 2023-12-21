fa4696fc85f44b067b021894841ae53333f2290e
Samarth-3 <spaliwal_be21@thapar.edu>
2023-12-21 12:27:59 +0530

    MongoDb Connected to Node.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const chats = require("./data/data");
 const dotenv = require("dotenv");
+dotenv.config();
+const connectDB = require("./config/db");
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
@@ -17,4 +20,4 @@ app.get("/api/chats/:id", (req, res) => {
 });
 
 const PORT= process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server runnign on post ${PORT}`));
+app.listen(PORT, console.log(`Server running on post ${PORT}`));
