bdf88aed62e21f0ff9a8a86b797c148578f7354d
Samarth-3 <spaliwal_be21@thapar.edu>
2024-01-04 12:14:28 +0530

    chat page ui (navbar,search user,individual chats)

---
+++ frontend/src/Context/ChatProvider.js
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const ChatContext = createContext("");
+
+const ChatProvider = ({ children }) => {
+  const [user, setUser] = useState();
+  const [selectedChat, setSelectedChat] = useState();
+  const [chats, setChats] = useState([]);
+  const navigate = useHistory();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userInfo = await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(JSON.parse(localStorage.getItem("userInfo")));
+        }, 0);
+      });
+      setUser(userInfo);
+      if (!userInfo) {
+        navigate.push("/");
+      }
+    };
+    fetchData();
+  }, [navigate]);
+
+  return (
+    <ChatContext.Provider
+      value={{ user, setUser, selectedChat, setSelectedChat, chats, setChats }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  const context = useContext(ChatContext);
+  return context;
+};
+
+export default ChatProvider;
