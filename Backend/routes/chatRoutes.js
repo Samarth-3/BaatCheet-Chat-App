f64fb5fb55ebb2f4824c5f4b83f50530a021394f
Samarth-3 <spaliwal_be21@thapar.edu>
2024-04-18 15:40:18 +0530

    Chats (adding user,removing in GC) and leave group

--- Backend/routes/chatRoutes.js
+++ Backend/routes/chatRoutes.js
@@ -15,7 +15,7 @@ router.post("/",protect, accessChat);
 router.get("/",protect, fetchChats);
 router.post("/group",protect, createGroupChat);
 router.put("/rename",protect, renameGroup);
-router.put("groupremove",protect, removeFromGroup);
-router.put("groupadd",protect, addToGroup);
+router.put("/groupremove",protect, removeFromGroup);
+router.put("/groupadd",protect, addToGroup);
 
 module.exports = router;
