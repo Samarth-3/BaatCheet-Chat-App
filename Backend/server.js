7a08d5203cb2335132e7d0d1d8a96f008202d95e
Samarth-3 <spaliwal_be21@thapar.edu>
2024-04-29 09:19:39 +0530

    Update server.js for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -10,15 +10,35 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+const path=require("path")
+
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
-app.get("/", (req, res) => {
-  res.send("API is running gg");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running gg");
+// });
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/messages", messageRoutes);
+
+//--------------------for deployment-------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+//--------------------------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
